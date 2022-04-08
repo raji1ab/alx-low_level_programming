75d89d5fa1204585c93c7811c5d0aab5a03b107e
Raji <rajanibongassie@gmail.com>
2022-04-08 11:30:52 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - reverse abc
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char h;
+
+for (h = 'z'; h >= 'a'; --h)
+{
+	putchar(h);
+}
+{
+	putchar('\n');
+}
+return (0);
+}
