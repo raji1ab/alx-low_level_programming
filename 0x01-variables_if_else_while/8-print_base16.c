0d734da50d8d3a63a95010c158b09d95fe801b9c
Raji <rajanibongassie@gmail.com>
2022-04-08 12:20:12 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - hexadecimal
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char x;
+
+for (x = '48'; x <= '57'; x++)
+{
+	putchar(x);
+}
+for (x = '97'; x <= '102'; x++)
+{
+	putchar(x);
+}
+{
+	putchar('\n');
+}
+return (0);
+}
