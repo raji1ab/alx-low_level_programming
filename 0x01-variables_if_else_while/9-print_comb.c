8597a2dc48bb5d26f3eff93eedcf75abacb50a2d
Raji <rajanibongassie@gmail.com>
2022-04-08 12:46:20 -0700

    commas ans spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - spaces and commas
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+for (n = 48; n <= 57; n++)
+{
+	putchar(n);
+}
+{
+	putchar(',');
+}
+{
+	putchar(' ');
+}
+{
+	putchar('\n');
+}
+return (0);
+}
