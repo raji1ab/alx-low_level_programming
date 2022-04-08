535f4ac8c9a657e750e6d7bcc0dac6ac39b86426
Raji <rajanibongassie@gmail.com>
2022-04-08 12:26:01 -0700

    hex01

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	char x;
 
-for (x = '48'; x <= '57'; x++)
+for (x = 48; x <= 57; x++)
 {
 	putchar(x);
 }
-for (x = '97'; x <= '102'; x++)
+for (x = 97; x <= 102; x++)
 {
 	putchar(x);
 }
