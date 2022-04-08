f09547b7bd9aebbde64741d596c8355dc0068843
Raji <rajanibongassie@gmail.com>
2022-04-08 09:56:18 -0700

    first no loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - Function taht prints 0 to 9
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+for (n = 0; n < 10; n++)
+{
+	printf("%d", n)
+}
+return (0);
+}
