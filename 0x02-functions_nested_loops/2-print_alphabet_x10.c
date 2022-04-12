bf735762920deedd5c97884f584aab66a6f0b5f8
Raji <rajanibongassie@gmail.com>
2022-04-12 09:13:41 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10 times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int r;
+
+	int num;
+
+	num = 0;
+
+	while (num < 10)
+	{
+		for ('r' = 'a'; 'r' <= 'z'; r++)
+	{
+
+			_putchar('r');
+	}
+
+
+	num++;
+
+	_putchar('\n');
+	}
+
+	return (0);
+}
