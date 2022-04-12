e5ad5de251e4ca8e6aad17fc837d807217610cad
Raji <rajanibongassie@gmail.com>
2022-04-12 08:35:09 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets
+ *
+ * Return: 0, always
+ */
+void print_alphabet(void)
+{
+	char b;
+
+	for ('b' = 'a'; 'b' <= 'z'; b++)
+	{
+		_putchar('b');
+	}
+
+	_putchar('\n');
+
+
+	return (0);
