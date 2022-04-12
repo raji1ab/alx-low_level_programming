3083028a74ea839485809973ba2b296426d527bd
Raji <rajanibongassie@gmail.com>
2022-04-12 12:31:13 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - finds sum
+ * @m: int to be added
+ * @n: int to be added
+ * Return: Always 0
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}
