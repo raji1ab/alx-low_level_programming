50a98785f47c13bbe613924949343d8239b7c58c
Raji <rajanibongassie@gmail.com>
2022-04-13 11:30:18 -0700

    first fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	long int i, f1 = 1, f2 = 2, sum = 0;
 
-	for (i = 0; i < 49; i++)
+	for (i = 0; i < 50; i++)
 	{
 		printf("%ld, ", f1);
 		sum = f1 + f2;
 		f1 = f2;
 		f2 = sum;
-	if (1 == 48)
+	if (1 == 49)
 		printf("%ld\n", f1);
 	}
 	return (0);
