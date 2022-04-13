07b988b62f20a2c8da4f5f20f727330a64ed3f9c
Raji <rajanibongassie@gmail.com>
2022-04-13 06:36:16 -0700

    task 5 twice

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,16 +9,16 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar ('+');
-		_putchar (',');
-		_putchar (' ');
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar ('0');
-		_putchar (',');
-		_putchar (' ');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		return (0);
 	}
 	else
@@ -28,5 +28,6 @@ int print_sign(int n)
 		_putchar (' ');
 		return (-1);
 	}
-}
 
+	return (0);
+}
