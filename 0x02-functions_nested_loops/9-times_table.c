e3193a68669591a69ccf7c2bd725b13409b2adea
Raji <rajanibongassie@gmail.com>
2022-04-13 12:08:05 -0700

    commit message

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,10 +4,8 @@
  * Return: Always 0.
  *
  */
-
 void times_table(void)
 {
-
 int a;
 
 int b;
@@ -23,12 +21,12 @@ if ((c / 10) == 0)
 {
 if (b == 0)
 {
-_putchar ('0');
+_putchar('0');
 }
 if (b != 0)
 {
-_putchar (' ');
-_putchar ((c % 10) + '0');
+_putchar(' ');
+_putchar((c % 10) + '0');
 }
 if (b < 9)
 {
@@ -37,8 +35,8 @@ _putchar (' ');
 }
 else
 {
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
 if (b < 9)
 {
 _putchar(',');
