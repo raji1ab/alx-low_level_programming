0a2be44ca74cc2f03a04bea820da33a48c9e588c
Raji <rajanibongassie@gmail.com>
2022-04-13 07:30:49 -0700

    task 008

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,20 +11,19 @@ void jack_bauer(void)
 
 	int b;
 
-	for ('a' = '0'; 'a' <= '23'; a++)
+	for (a = '0'; a <= '23'; a++)
 	{
-		for ('b' = '0'; 'b' <= '23'; b++)
+		for (b = '0'; b <= '23'; b++)
 		{
-			_putchar (a / 10 + '0');
-			_putchar (a % 10 + '0');
-			_putchar (':');
-			_putchar (b / 10 + '0');
-			_putchar (b % 10 + '0');
-			_putchar ('\n');
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
 		}
 
 	}
 
-	return (0);
 
 }
