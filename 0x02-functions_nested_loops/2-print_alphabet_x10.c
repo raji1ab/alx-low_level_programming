dfc6ea52735f860f26b0fd0f0378c48e9936f381
Raji <rajanibongassie@gmail.com>
2022-04-13 02:57:31 -0700

    task 2 twice

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,10 +16,10 @@ void print_alphabet_x10(void)
 
 	while (num < 10)
 	{
-		for ('r' = 'a'; 'r' <= 'z'; r++)
+		for (r = 'a'; r <= 'z'; r++)
 	{
 
-			_putchar ('r');
+			_putchar (r);
 	}
 
 
@@ -28,5 +28,4 @@ void print_alphabet_x10(void)
 	_putchar ('\n');
 	}
 
-	return (0);
 }
