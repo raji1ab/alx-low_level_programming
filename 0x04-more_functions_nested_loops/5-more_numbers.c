f9b4a929b3caa61f1338140fb2817c40e71baad8
Raji <rajanibongassie@gmail.com>
2022-04-14 08:38:38 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,7 @@ int a;
 
 int num;
 
-num = 0;
-
-while (num < 10)
+for (num = 0; num < 10; num++)
 {
 for (a = 0; a <= 14; a++)
 {
@@ -22,7 +20,6 @@ _putchar((a / 10) + '0');
 }
 _putchar((a % 10) + '0');
 }
-num++
 _putchar(10);
 }
 }
