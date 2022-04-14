e4816f0b40adf5c95986ac9c8320d85a86f95cd3
Raji <rajanibongassie@gmail.com>
2022-04-14 09:58:13 -0700

    tak 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print  numbers 1 to 100 except for multiples of 3 and 5
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0 && a % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+
+		printf("%d", a);
+	}
+	return (0);
+}
