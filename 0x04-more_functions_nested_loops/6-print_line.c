0e599120ff14d037969928f1cb84128209ab6f26
Raji <rajanibongassie@gmail.com>
2022-04-14 08:57:40 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - prints a traight line
+ * @n: the number of times _ will be printed
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a;
+
+		for (a = 1; a <= n; a++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
