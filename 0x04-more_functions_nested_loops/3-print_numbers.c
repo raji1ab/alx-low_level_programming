bcd151cc6f0a67552db1b9b1ebc21391674b783d
Raji <rajanibongassie@gmail.com>
2022-04-14 06:51:43 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints numbers
+ *
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+}
