2a4392e25927e8d546a90b6a950a1142e9609ad9
Raji <rajanibongassie@gmail.com>
2022-04-14 08:34:26 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,22 +6,23 @@
  */
 void more_numbers(void)
 {
-	int a;
+int a;
 
-	int num;
+int num;
 
-	num = 0;
-	while (num < 10)
-	{
-		for (a = 0; a <= 14; a++)
-		{
-			if (a > 9)
-			{
-				_putchar((a / 10) + '0');
-			}
-			_putchar((a % 10) + '0');
-		}
-		num++
-		_putchar(10);
-	}
+num = 0;
+
+while (num < 10)
+{
+for (a = 0; a <= 14; a++)
+{
+if (a > 9)
+{
+_putchar((a / 10) + '0');
+}
+_putchar((a % 10) + '0');
+}
+num++
+_putchar(10);
+}
 }
