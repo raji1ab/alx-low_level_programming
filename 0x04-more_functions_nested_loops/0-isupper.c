964df62305d7e24e79a9c50277c944f3dbada19c
Raji <rajanibongassie@gmail.com>
2022-04-14 02:02:02 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - identifies upper case character
+ * @c: character to be identified
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}
