214e561920d7172c4d3689450e201e1f171c57d6
Raji <rajanibongassie@gmail.com>
2022-04-18 11:28:17 -0700

    Advanced task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _atoi - function that converts string to an integer
+ *
+ * @s: string to be converted
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int sign = 1, i = 0;
+
+	unsigned int res = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	res *= sign
+	return (res);
+}
