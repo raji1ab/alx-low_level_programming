9736fa8b3a599e18188f14b092bb40a62062b6fb
Raji <rajanibongassie@gmail.com>
2022-04-18 08:47:49 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - fuction that prints character followed by a new line
+ * @str: name of the string
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	_putchar(srt[i]);
+	_putchar('\n');
+	}
+}
