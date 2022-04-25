071bbc014eee5a377be31933442092ea59fe57d5
Raji <rajanibongassie@gmail.com>
2022-04-25 07:49:13 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,8 +10,10 @@ char *_strchr(char *s, char c)
 	unsigned int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-
+	{
 		if (s[i] == c)
 			break;
-		return (s[i] == c ? (s + i) : '\0');
+	}
+	return (s[i] == c ? (s + i) : '\0');
+
 }
