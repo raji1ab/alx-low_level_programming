270822645ead0c0233a150134ab8afbcd2afd1c6
Raji <rajanibongassie@gmail.com>
2022-04-25 08:21:04 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,6 @@ char *_strstr(char *haystack, char *needle)
 	{
 		k = 0;
 		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
-
 			haystack++, needle++, k++;
 			if (*needle == '\0')
 				return (haystack - k);
