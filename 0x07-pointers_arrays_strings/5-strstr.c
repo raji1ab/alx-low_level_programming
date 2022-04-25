4b22a0dd236b90802cd5ae8208032e6b46fa8aec
Raji <rajanibongassie@gmail.com>
2022-04-25 08:18:53 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr - locates a sub string
+ * @haystack: input string
+ * @needle: string to find
+ * Return: pointer to the located sub string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int k;
+
+	while (*haystack != '\0')
+	{
+		k = 0;
+		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+
+			haystack++, needle++, k++;
+			if (*needle == '\0')
+				return (haystack - k);
+			haystack -= (k - 1), needle -= k;
+	}
+	return ('\0');
+}
