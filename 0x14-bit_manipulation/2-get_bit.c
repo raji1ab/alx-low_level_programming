7dbac5eb7417376bbba3960bb6e17fe60012ac61
Raji <rajanibongassie@gmail.com>
2022-05-26 06:30:11 -0700

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - function with two arguments
+ * @n: value to compare
+ * @index: postion to compare
+ *
+ * Description: returns the value of a bit at a given index
+ * Return: 0 or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8))
+		return (-1);
+
+	n  = n >> index;
+	if (n & 1)
+		return (1);
+	else
+		return (0);
+}
